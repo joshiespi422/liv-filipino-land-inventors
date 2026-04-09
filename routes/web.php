e4c485f82d983bc341932643dfcb548bd9b8d576
joshi <jespadilla422@gmail.com>
2026-04-09 17:58:30 +0800

    feat(business-training): add controller, route, page

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
 use App\Models\UserType;
+use App\Http\Controllers\Web\BusinessTrainingController;
 
 Route::inertia('/', 'Home', [
     'canRegister' => Features::enabled(Features::registration()),
@@ -13,6 +14,12 @@ Route::middleware([
     'role:' . UserType::SUPER_ADMIN . ',' . UserType::ADMIN
 ])->group(function () {
     Route::inertia('/dashboard', 'Dashboard')->name('dashboard');
+
+    // Business Training Domain
+    Route::middleware(['service_access:business-training'])->group(function () {
+        Route::get('/business-training', [BusinessTrainingController::class, 'index'])->name('business-training.index');
+    });
+
 });
 
 require __DIR__.'/settings.php';
