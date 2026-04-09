bdd4d745ccdbaa16f1eaeb999ec5d91abd537c1c
Iverson Mamangun <177076443+IversonMamangun@users.noreply.github.com>
2026-04-09 11:58:36 +0800

    fix(Home): fimport/order linting error

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,17 +1,17 @@
 <script setup lang="ts">
 // 1. Tell Inertia NOT to use the default dashboard layout
-defineOptions({ layout: null as any });
+// defineOptions({ layout: null as any });
 // import { Head, Link } from '@inertiajs/vue3';
 // import { dashboard, login, register } from '@/routes';
-import HomeSection from '@/components/landing/HomeSection.vue';
 import AboutSection from '@/components/landing/AboutSection.vue';
+import ContactSection from '@/components/landing/ContactSection.vue';
+import Footer from '@/components/landing/Footer.vue';
+import GallerySection from '@/components/landing/GallerySection.vue';
+import HomeSection from '@/components/landing/HomeSection.vue';
+import NewsUpdates from '@/components/landing/NewsUpdates.vue';
 import ProgramServices from '@/components/landing/ProgramServices.vue';
 import StrategicPlans from '@/components/landing/StrategicPlans.vue';
 import TestimonialsSection from '@/components/landing/TestimonialsSection.vue';
-import NewsUpdates from '@/components/landing/NewsUpdates.vue';
-import GallerySection from '@/components/landing/GallerySection.vue';
-import ContactSection from '@/components/landing/ContactSection.vue';
-import Footer from '@/components/landing/Footer.vue';
 
 withDefaults(
     defineProps<{
