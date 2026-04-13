90503101413047c5e7fe88b3e48ec0da4e43ff95
Ahllaine Christian De Ocera <acdeocera.dev@gmail.com>
2026-04-13 10:02:53 +0800

    fix(request): update the request for user registering and not completed.

--- app/Http/Requests/Auth/RegisterRequest.php
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -27,7 +27,7 @@ class RegisterRequest extends FormRequest
     {
         return [
             'name' => ['required', 'string', 'max:255'],
-            'phone' => ['required', 'string', 'max:20', 'unique:' . User::class, 'unique:' . PendingRegistration::class],
+            'phone' => ['required', 'string', 'max:20', 'unique:' . User::class],
         ];
     }
 }
