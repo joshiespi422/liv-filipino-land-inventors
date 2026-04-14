acf4dc2727341a965a63b125eb0171717861beff
joshi <jespadilla422@gmail.com>
2026-04-14 18:28:34 +0800

    feat: add sonner toast package

--- resources/js/layouts/AppLayout.vue
+++ resources/js/layouts/AppLayout.vue
@@ -1,14 +1,17 @@
 <script setup lang="ts">
 import AppLayout from '@/layouts/app/AppSidebarLayout.vue';
 import type { BreadcrumbItem } from '@/types';
+import { Toaster } from '@/components/ui/sonner';
+import 'vue-sonner/style.css';
 
 const { breadcrumbs = [] } = defineProps<{
-    breadcrumbs?: BreadcrumbItem[];
+  breadcrumbs?: BreadcrumbItem[];
 }>();
 </script>
 
 <template>
-    <AppLayout :breadcrumbs="breadcrumbs">
-        <slot />
-    </AppLayout>
+  <AppLayout :breadcrumbs="breadcrumbs">
+    <slot />
+    <Toaster />
+  </AppLayout>
 </template>
