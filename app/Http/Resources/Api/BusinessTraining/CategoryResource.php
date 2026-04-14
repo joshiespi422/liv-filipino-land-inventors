b7c3413dd8e333239a953af65e82db0093e462cd
Ahllaine Christian De Ocera <acdeocera.dev@gmail.com>
2026-04-14 11:46:51 +0800

    fix(resource): added icon for getting single category for business training api

--- app/Http/Resources/Api/BusinessTraining/CategoryResource.php
+++ app/Http/Resources/Api/BusinessTraining/CategoryResource.php
@@ -25,4 +25,16 @@ class CategoryResource extends JsonApiResource
         'type' => TypeResource::class,
         'trainings' => TrainingResource::class,
     ];
+
+    public function toAttributes(Request $request): array
+    {
+        return [
+            'name' => $this->name,
+            'slug' => $this->slug,
+            'description' => $this->description,
+            'icon' => $this->whenLoaded('type', fn() => $this->type->icon),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
 }
