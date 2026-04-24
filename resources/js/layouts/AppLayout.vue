d3546a960a6d1c54cdc38101d2b491f2237886a9
IversonMamangun <177076443+IversonMamangun@users.noreply.github.com>
2026-04-24 11:32:59 +0800

    refactor(imports): reorganize imports to satisfy eslint rules

--- resources/js/layouts/AppLayout.vue
+++ resources/js/layouts/AppLayout.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
+import { Toaster } from '@/components/ui/sonner';
 import AppLayout from '@/layouts/app/AppSidebarLayout.vue';
 import type { BreadcrumbItem } from '@/types';
-import { Toaster } from '@/components/ui/sonner';
 import 'vue-sonner/style.css';
 
 const { breadcrumbs = [] } = defineProps<{
