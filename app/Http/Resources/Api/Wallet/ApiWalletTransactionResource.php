38d9f4406bfbcd3b8f2d6f6845852c8567e266d8
Jospay <joshuapayumo2001@gmail.com>
2026-09-19 16:51:55 +0800

    feat(wallet): add fee and account details to wallet transaction resource

--- app/Http/Resources/Api/Wallet/ApiWalletTransactionResource.php
+++ app/Http/Resources/Api/Wallet/ApiWalletTransactionResource.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Resources\Api\Wallet;
 
-use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class ApiWalletTransactionResource extends JsonResource
@@ -17,10 +16,16 @@ class ApiWalletTransactionResource extends JsonResource
         return [
             'id' => $this->id,
             'amount' => number_format($this->amount, 2, '.', ''),
+            'transfer_fee' => number_format($this->transfer_fee ?? 0, 2, '.', ''),
             'type' => $this->type,
+            'from_name' => $this->from_name,
+            'to_account_name' => $this->to_account_name,
+            'to_account_number' => $this->to_account_number,
+            'to_provider' => $this->to_provider,
             'description' => $this->description,
             'reference_id' => $this->reference_id,
             'reference_type' => $this->reference_type,
+            'reference_number' => $this->reference_number,
             'created_at' => $this->created_at->toDateTimeString(),
         ];
     }
