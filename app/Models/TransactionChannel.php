9ef0f1f50b4c611dd8373fa4771512ac5334aa64
Jospay <joshuapayumo2001@gmail.com>
2026-09-19 16:52:21 +0800

    feat(transfer): add active query scope to TransactionChannel model

--- app/Models/TransactionChannel.php
+++ app/Models/TransactionChannel.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class TransactionChannel extends Model
@@ -18,4 +19,9 @@ class TransactionChannel extends Model
     protected $casts = [
         'is_active' => 'boolean',
     ];
+
+    public function scopeActive(Builder $query): Builder
+    {
+        return $query->where('is_active', true);
+    }
 }
